a7b95d9dc2dbb723508df1afc6d267bc57f086fc
Charles Gery <charles.gery@gmail.com>
2021-02-02 14:27:47 +0100

    Fix team name error

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -26,7 +26,7 @@ public class QueryProcessorTest {
     }
 
     @Test
-    public void isNotCaseSensitive() throws Exception {
+    public void teamName() throws Exception {
         assertThat(queryProcessor.process("what is your name"), containsString("Piazza"));
     }
 }
